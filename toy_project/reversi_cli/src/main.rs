df9cf54936c31078204a3f51d77588605aaf5cee
r3dzone <coolitop123@gmail.com>
2021-01-31 16:51:15 +0900

    add stone_state array & modified print_board func

--- toy_project/reversi_cli/src/main.rs
+++ toy_project/reversi_cli/src/main.rs
@@ -1,9 +1,22 @@
 fn main() {
+   let mut stone_state :[[i8;8];8] = [[3;8];8]; //white:0 black:1 none:3
+    
+    //stone initialize;
+    stone_state[3][3] = 0;
+    stone_state[3][4] = 1;
+    stone_state[4][3] = 1;
+    stone_state[4][4] = 0;
+
+    for i in 0..8{
+        for j in 0..8{
+            print!("{} ",stone_state[i][j]);
+        }println!("");
+    }
     title();
-    print_board();
+    print_board(&stone_state);
 }
 
-fn print_board(){
+fn print_board(stone_state : &[[i8;8];8]){
     //black = ● white = ○ ┼ ─ │
     let line = "┼───┼───┼───┼───┼───┼───┼───┼───┼";
     let col_idx = ['A','B','C','D','E','F','G','H'];
@@ -14,7 +27,14 @@ fn print_board(){
     for i in 0..8 {
         print!(" {}\n{}",line,i+1);
         for j in 0..8{
-            print!("│ {} ",j);
+            let stone = if stone_state[i][j] == 0 {
+                "○"
+            }else if stone_state[i][j] == 1{
+                "●"
+            }else{
+                " "
+            };
+            print!("│ {} ",stone);
         }print!("│\n");
     }
     println!(" {}",line);
