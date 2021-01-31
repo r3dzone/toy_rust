bd44bd87dafb7c191d2e92cc83a384a56798ef08
r3dzone <coolitop123@gmail.com>
2021-01-31 16:10:35 +0900

    add print_board func

---
+++ toy_project/reversi_cli/target/debug/deps/reversi_cli.d
@@ -0,0 +1,5 @@
+/Users/r3dzone/Desktop/project/rust/toy_project/reversi_cli/target/debug/deps/reversi_cli: src/main.rs
+
+/Users/r3dzone/Desktop/project/rust/toy_project/reversi_cli/target/debug/deps/reversi_cli.d: src/main.rs
+
+src/main.rs:
---
+++ toy_project/reversi_cli/target/debug/reversi_cli.d
@@ -0,0 +1,1 @@
+/Users/r3dzone/Desktop/project/rust/toy_project/reversi_cli/target/debug/reversi_cli: /Users/r3dzone/Desktop/project/rust/toy_project/reversi_cli/src/main.rs
